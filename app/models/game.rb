91ebb380e912e85a3e0f320bb579f7c3942ac55f
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-04 10:41:32 -0300

    Game should validate that the length of :user_name is at most 20

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   validates :user_name, presence: true
+  validates :user_name, length: { maximum: 20 }
   validates :score, numericality: { less_than_or_equal_to: 300 }
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of :user_name }
+    it { is_expected.to validate_length_of(:user_name).is_at_most(20) }
     it { is_expected.to validate_numericality_of :score }
     it {
       is_expected.to validate_numericality_of(:score)
